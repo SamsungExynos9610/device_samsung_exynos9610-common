8a58f7d192a4b8fec1b2ab0821979a99cfe26b30
Dylan Neve <dylanneve1@gmail.com>
2022-09-02 12:57:39 +0100

    exynos9610-common: vendorsetup: drop health service override
    * We should not replace init files manually for default services

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,11 +10,6 @@ cd -
 # Light patch 
 cd frameworks/base && wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices-a12l.patch && patch -p1 < *.patch && cd -
 
-# Health fix
-
-sudo rm -r ./hardware/interfaces/health/2.1/default/android.hardware.health@2.1-service.rc
-cp ./device/samsung/exynos9610-common/patch/android.hardware.health@2.1-service.rc ./hardware/interfaces/health/2.1/default/
-
 # BT calls fix 
 
 cd system/bt;
